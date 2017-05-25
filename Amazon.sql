972c29bdc5fe1a07ff1a4fb3ec09fa18b0d40384
Cowfactory <iisdndr@gmail.com>
2017-05-24 18:44:24 -0700

    copied sample data code into Amazon.sql for turn in purpose

--- Amazon.sql
+++ Amazon.sql
@@ -9,7 +9,7 @@
 
 
 CREATE TABLE ACCOUNT (
-    accID INT	            NOT NULL,
+    accID 		INT	            NOT NULL,
     Fname		VARCHAR(20)     NOT NULL,
     Lname		VARCHAR(20)     NOT NULL,
     region 		VARCHAR(50)     NOT NULL,
@@ -96,3 +96,28 @@ CREATE TABLE EBOOKS(
         ON UPDATE CASCADE,
 	CHECK	(starRating >= 1 AND starRating <= 5)
 );
+
+--Sample data
+
+--ACCOUNTS sample data
+INSERT INTO ACCOUNTS 	
+	VALUES (1765, ‘Carey’, ‘Roberts’, ‘Pacific Northwest’, 25);
+
+--GAMES sample data
+INSERT INTO GAMES 
+	VALUES ( 0000001, ‘Final Fantasy VII’, ‘SQUARESOFT’, ‘RPG’, ‘T’, NULL, NULL);
+INSERT INTO GAMES 
+	VALUES ( 0000002, ‘Final Fantasy XV’, ‘SQUARE ENIX’, ‘RPG’, ‘T’, 5, 50:14:23);
+INSERT INTO GAMES 
+	VALUES ( 0000000, ‘BROKEN GAMEI’, ‘SQUARELUL’, ‘jRPG’, ‘T’, 6, NULL);
+
+--MOVIES sample data
+INSERT INTO MOVIES
+	VALUES (5504, ‘Casablanca’, 11/26/1942, ‘Michael Curtiz’, ‘Drama Film, Romance’ , 01:42:67, ‘Hal B. Wallis’, 1765, ‘PG’, 5);
+
+--MUSIC sample data	
+INSERT INTO MUSIC 
+	VALUES ( 7145, ‘Apologize’, ‘One Republic’ , ‘Greg Wells’, 00:03:28, 5, 1765);
+
+
+--EBOOKS sample data
