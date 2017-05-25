1f10066abb756e22f1685d3e5311bb90024c5f46
Cowfactory <iisdndr@gmail.com>
2017-05-24 19:24:11 -0700

    fixed queries

--- Amazon.sql
+++ Amazon.sql
@@ -48,15 +48,15 @@ CREATE TABLE MOVIES(
     publisher 		VARCHAR(15) 	NOT NULL,
     accID 			INT 			NOT NULL,
     starRating 		INT,
-    ageRating       VARCHAR(5),
+    ageRating       VARCHAR(5)      NOT NULL DEFAULT('UNR'),
     PRIMARY KEY (movieID),
     FOREIGN KEY (accID) 
         REFERENCES ACCOUNT(accID) 
         ON UPDATE CASCADE,
     CHECK (starRating >= 1 AND starRating <= 5),
-    CHECK (ageRating = ('eC') OR ageRating = ('E') OR 
-          ageRating = ('E10') OR ageRating = ('T')  OR 
-          ageRating = ('M')  OR ageRating = ('Ao'))
+    CHECK (ageRating = ('G') OR ageRating = ('PG') OR 
+	ageRating = ('PG-13') OR ageRating = ('R')  
+	OR ageRating = ('NC-17')  OR ageRating = ('UNR'))
 );
 
 CREATE TABLE CASTLIST(
@@ -100,24 +100,24 @@ CREATE TABLE EBOOKS(
 --Sample data
 
 --ACCOUNTS sample data
-INSERT INTO ACCOUNTS 	
-	VALUES (1765, ‘Carey’, ‘Roberts’, ‘Pacific Northwest’, 25);
+INSERT INTO ACCOUNT 	
+	VALUES (1765, 'Carey', 'Roberts', 'Pacific Northwest', 25);
 
 --GAMES sample data
 INSERT INTO GAMES 
-	VALUES ( 0000001, ‘Final Fantasy VII’, ‘SQUARESOFT’, ‘RPG’, ‘T’, NULL, NULL);
+	VALUES ( 0000001, 'Final Fantasy VII', 'SQUARESOFT', 'RPG', 'T', NULL, NULL, 1765);
 INSERT INTO GAMES 
-	VALUES ( 0000002, ‘Final Fantasy XV’, ‘SQUARE ENIX’, ‘RPG’, ‘T’, 5, 50:14:23);
+	VALUES ( 0000002, 'Final Fantasy XV', 'SQUARE ENIX', 'RPG', 'T', 5, '50:14:23', 1765);
 INSERT INTO GAMES 
-	VALUES ( 0000000, ‘BROKEN GAMEI’, ‘SQUARELUL’, ‘jRPG’, ‘T’, 6, NULL);
+	VALUES ( 0000000, 'BROKEN GAMEI', 'SQUARELUL', 'jRPG', 'T', 6, NULL, 42069);
 
 --MOVIES sample data
 INSERT INTO MOVIES
-	VALUES (5504, ‘Casablanca’, 11/26/1942, ‘Michael Curtiz’, ‘Drama Film, Romance’ , 01:42:67, ‘Hal B. Wallis’, 1765, ‘PG’, 5);
+	VALUES (5504, 'Casablanca', '1942-11-26', 'Michael Curtiz', 'Drama Film, Romance' , '01:42:67', 'Hal B. Wallis', 1765, 5, 'PG');
 
 --MUSIC sample data	
 INSERT INTO MUSIC 
-	VALUES ( 7145, ‘Apologize’, ‘One Republic’ , ‘Greg Wells’, 00:03:28, 5, 1765);
+	VALUES ( 7145, 'Apologize', 'One Republic' , 'Greg Wells', '00:03:28', 5, 1765);
 
 
 --EBOOKS sample data
