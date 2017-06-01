6e50c6b6fcb8550bef39eda2bd23ac575561590f
Cowfactory <iisdndr@gmail.com>
2017-05-31 17:21:41 -0700

    updated colons

--- DataAndQueries.sql
+++ DataAndQueries.sql
@@ -2,23 +2,38 @@
 
 --ACCOUNTS sample data
 INSERT INTO ACCOUNTS 	
-	VALUES (1765, ‘Carey’, ‘Roberts’, ‘Pacific Northwest’, 25);
+	VALUES (1765, 'Carey', 'Roberts', 'Pacific Northwest', 25);
 
 --GAMES sample data
 INSERT INTO GAMES 
-	VALUES ( 0000001, ‘Final Fantasy VII’, ‘SQUARESOFT’, ‘RPG’, ‘T’, NULL, NULL);
+	VALUES ( 0000001, 'Final Fantasy VII', 'SQUARESOFT', 'RPG', 'T', NULL, NULL);
 INSERT INTO GAMES 
-	VALUES ( 0000002, ‘Final Fantasy XV’, ‘SQUARE ENIX’, ‘RPG’, ‘T’, 5, 50:14:23);
+	VALUES ( 0000002, 'Final Fantasy XV', 'SQUARE ENIX', 'RPG', 'T', 5, 50:14:23);
 INSERT INTO GAMES 
-	VALUES ( 0000000, ‘BROKEN GAMEI’, ‘SQUARELUL’, ‘jRPG’, ‘T’, 6, NULL);
+	VALUES ( 0000000, 'BROKEN GAMEI', 'SQUARELUL', 'jRPG', 'T', 6, NULL);
 
 --MOVIES sample data
 INSERT INTO MOVIES
-	VALUES (5504, ‘Casablanca’, 11/26/1942, ‘Michael Curtiz’, ‘Drama Film, Romance’ , 01:42:67, ‘Hal B. Wallis’, 1765, ‘PG’, 5);
+	VALUES (5504, 'Casablanca', 11/26/1942, 'Michael Curtiz', 'Drama Film, Romance' , 01:42:67, 'Hal B. Wallis', 1765, 'PG', 5);
 
 --MUSIC sample data	
 INSERT INTO MUSIC 
-	VALUES ( 7145, ‘Apologize’, ‘One Republic’ , ‘Greg Wells’, 00:03:28, 5, 1765);
+	VALUES ( 7145, 'Apologize', 'One Republic' , 'Greg Wells', 00:03:28, 5, 1765);
 
 
 --EBOOKS sample data
+
+INSERT INTO EBOOKS
+	VALUES(1566, 'Lion', 'Saroo Brierley', 'Joseph Ellis', 5, 1765);
+INSERT INTO EBOOKS
+	VALUES(1567, 'Sully', 'Harper Collins', 'Jeffrey Zaslow', 5, 1766);
+INSERT INTO EBOOKS
+	VALUES(1568, 'Hidden Figures', 'Margot Lee', 'Arthur Zelizer', 5, 1767);
+
+--CASTLIST sample data
+INSERT INTO CASTLIST 
+	VALUES ('Humphrey', 'Bogart', 7145);
+INSERT INTO CASTLIST
+	VALUES ('Daniel', 'Radcliffe', 7146);
+INSERT INTO CASTLIST
+	VALUES('Leonardo', 'DiCaprio', 7147);
